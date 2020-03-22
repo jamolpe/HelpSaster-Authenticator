cf809451ef88a11f7adc0cc8ce5980603a34b932
jamolpe <jamolpe@gmail.com>
2020-03-22 21:28:49 +0100

    added the postman test collection

--- internal/api/session-handler.go
+++ internal/api/session-handler.go
@@ -9,9 +9,9 @@ import (
 
 // CheckValidSession : we check if the session is valid
 func (api *API) CheckValidSession(c echo.Context) error {
-	cookie, err := c.Cookie("HelpSasterAuth")
+	cookie, err := c.Cookie("help_saster")
 	if err != nil {
-		c.JSON(500, models.ErrorResponse{Code: 005, Message: "cookie not found need to relogin"})
+		return c.JSON(500, models.ErrorResponse{Code: 005, Message: "cookie not found need to relogin"})
 	}
 	fmt.Println(cookie.Name)
 	fmt.Println(cookie.Value)
