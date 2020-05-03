93dd9f58834510a96cbdcd64d5328475c6653364
jamolpe <jamolpe@gmail.com>
2020-05-03 22:45:42 +0200

    improve the cookie managment

--- internal/api/cookie.go
+++ internal/api/cookie.go
@@ -1,32 +1,61 @@
 package api
 
 import (
-	b64 "encoding/base64"
 	"encoding/json"
 	"go-sessioner/pkg/models"
 	"net/http"
+	"time"
+
+	guuid "github.com/google/uuid"
+
+	"github.com/gorilla/securecookie"
 )
 
-func createCookie(userInfo models.AuthUser) (*http.Cookie, error) {
-	cookie := new(http.Cookie)
-	cookie.Name = "help_saster"
-	var userSessionInfo, err = json.Marshal(userInfo)
-	if err != nil {
-		return nil, err
-	}
-	cookie.Domain = "localhost"
-	cookie.Value = b64.StdEncoding.EncodeToString(userSessionInfo)
-	cookie.Secure = false
-	return cookie, nil
+var hashKey = []byte(securecookie.GenerateRandomKey(32))
+var blockKey = []byte(securecookie.GenerateRandomKey(32))
+var s = securecookie.New(hashKey, blockKey)
+
+func mapAuthUserToSession(authUser models.AuthUser, token string) models.Session {
+	var session models.Session
+	session.CreatedAt = time.Now()
+	session.Token = token
+	session.User = authUser.User
+	session.ID = guuid.New()
+	return session
 }
 
-func decodeCookie(cookie *http.Cookie) (*models.AuthUser, error) {
-	value := cookie.Value
-	authUser := new(models.AuthUser)
-	decodedValue, _ := b64.StdEncoding.DecodeString(value)
-	err := json.Unmarshal([]byte(decodedValue), authUser)
+func createCookie(session models.Session) (*http.Cookie, error) {
+	var userSessionInfo, err = json.Marshal(session)
 	if err != nil {
 		return nil, err
 	}
-	return authUser, nil
+	value := map[string]string{
+		"session": string(userSessionInfo),
+	}
+	if encoded, err := s.Encode("help_saster", value); err == nil {
+		cookie := &http.Cookie{
+			Name:     "help_saster",
+			Value:    encoded,
+			Path:     "/",
+			Secure:   true,
+			HttpOnly: true,
+		}
+		return cookie, nil
+	} else {
+		return nil, nil
+	}
+}
+
+func decodeCookie(cookie *http.Cookie) (*models.Session, error) {
+	value := make(map[string]string)
+	session := new(models.Session)
+	if err := s.Decode("help_saster", cookie.Value, &value); err == nil {
+		jsonerror := json.Unmarshal([]byte(value["session"]), session)
+		if jsonerror != nil {
+			return nil, jsonerror
+		}
+		return session, nil
+	} else {
+		return nil, err
+	}
 }
